b34176a60e34a96bcb63e2ef2bad449ee49f0b3a
JayPeKs <156256548+JayPeKs@users.noreply.github.com>
2024-11-27 18:36:26 +0100

    first working menu created

---
+++ code/game/src/guiElements/battleMenu.cpp
@@ -0,0 +1,8 @@
+//
+// Created by julia on 27/11/2024.
+//
+
+#include "battleMenu.h"
+
+namespace gl3 {
+} // gl3
---
+++ code/game/src/guiElements/battleMenu.h
@@ -0,0 +1,13 @@
+//
+// Created by julia on 27/11/2024.
+//
+
+#pragma once
+
+namespace gl3 {
+
+class battleMenu {
+
+};
+
+} // gl3
