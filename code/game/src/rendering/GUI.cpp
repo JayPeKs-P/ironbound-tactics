4580bb74ffc25cb7f53d31e8e6f49eeca358ec65
Julian K. <julian.kalb@stud-mail.uni-wuerzburg.de>
2025-01-21 10:34:31 +0100

    gui started refactoring

---
+++ code/game/src/rendering/GUI.cpp
@@ -0,0 +1,8 @@
+//
+// Created by julia on 19/01/2025.
+//
+
+#include "GUI.h"
+
+namespace gl3 {
+} // gl3
---
+++ code/game/src/rendering/GUI.h
@@ -0,0 +1,13 @@
+//
+// Created by julia on 19/01/2025.
+//
+
+#pragma once
+
+namespace gl3 {
+
+class GUI {
+
+};
+
+} // gl3
