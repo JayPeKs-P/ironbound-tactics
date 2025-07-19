b925041d3e3e5619166a1420de18d9b3cbad71b8
JayPeKs-P <juliankalb@gmx.de>
2025-07-19 14:28:16 +0200

    add new dummy class CombatFunctions

---
+++ code/game/src/logic/CombatFunctions.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Julian Kalb on 19/07/2025.
+//
+
+#include "CombatFunctions.h"
+
+namespace gl3 {
+} // gl3
---
+++ code/game/src/logic/CombatFunctions.h
@@ -0,0 +1,13 @@
+//
+// Created by Julian Kalb on 19/07/2025.
+//
+
+#pragma once
+
+namespace gl3 {
+
+class CombatFunctions {
+
+};
+
+} // gl3
