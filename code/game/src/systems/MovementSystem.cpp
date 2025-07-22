c3847e5979a482d404790e3675c173d9c4fdfbc9
JayPeKs-P <juliankalb@gmx.de>
2025-07-22 15:47:11 +0200

    add moveCurved function, that makes all instances of a unit move to a given position in a curve (for projectiles)

--- code/game/src/systems/MovementSystem.cpp
+++ code/game/src/systems/MovementSystem.cpp
@@ -4,6 +4,8 @@
 
 #include "MovementSystem.h"
 
+#include <iostream>
+
 #include "../components/UnitState.h"
 #include "engine/sceneGraph/Transform.h"
 #include "../components/unitTypes/Unit.h"
@@ -25,15 +27,23 @@ namespace gl3 {
                 auto unitCategory = game.componentManager.getComponent<Unit>(transform.entity()).category;
                 if (unitCategory == UnitCategory::INFANTRY)
                 {
-                    move(transform, goalPositon, deltaTime);
+                    glm::vec3 direction = goalPositon - transform.localPosition;
+                    bool finished = false;
+                    // finished = moveStraight(transform, direction, deltaTime);
+                    finished = moveCurved(transform, glm::vec3(1.75f, 0.0f ,0.0f), 1.0f, deltaTime);
+                    if (finished)
+                    {
+                        std::cout << "finished"<<std::endl;
+                        finished = false;
+                    }
                 }
             }
         });
     }
 
-    void MovementSystem::move(Transform& root, glm::vec3 goalPosition, float deltatime)
+    bool MovementSystem::moveStraight(Transform& root, glm::vec3 direction, float deltatime)
     {
-        glm::vec3 direction = goalPosition - root.localPosition;
+        bool reached = false;
         float distanceToGoal = glm::length(direction);
         auto children = root.getChildTransforms();
         for (auto& childTransform : children)
@@ -46,7 +56,59 @@ namespace gl3 {
             {
                 childTransform->localPosition += speed * deltatime * direction;
                 unitState_C.traveledDistance += 2* deltatime * speed;
+            }else
+            {
+                unitState_C.state = State::IDLE;
+                reached = true;
             }
         }
+        return reached;
+    }
+
+    bool MovementSystem::moveCurved(Transform& root, glm::vec3 goal, float compression, float deltatime)
+    {
+        bool reached = false;
+
+        glm::vec3 start = root.localPosition;
+        glm::vec3 forward = glm::normalize(goal - start);
+        glm::vec3 worldUp = glm::vec3(0, 1, 0);
+
+        glm::vec3 side = glm::normalize(glm::cross(forward, worldUp));
+        glm::vec3 up = glm::normalize(glm::cross(side, forward));
+
+        glm::vec3 mid = (start + goal) * 0.5f + compression * up;
+        float totalDistance = glm::length(goal - start);
+
+        glm::vec3 direction = goal - root.localPosition;
+        float distanceToGoal = glm::length(direction);
+        auto children = root.getChildTransforms();
+        for (auto& childTransform : children)
+        {
+            auto& unitState_C = engine.componentManager.getComponent<UnitState>(childTransform->entity());
+            auto speed = 3.0f;
+
+            float t = unitState_C.traveledDistance / totalDistance;
+            if (t > 1.0f) t = 1.0f;
+
+
+            if (distanceToGoal - unitState_C.traveledDistance >= 0)
+            {
+                float t = unitState_C.traveledDistance / totalDistance;
+                if (t > 1.0f) t = 1.0f;
+
+                glm::vec3 direction =
+                    2.0f * (1.0f - t) * (mid - start) +
+                    2.0f * t * (goal - mid);
+                direction = glm::normalize(direction);
+
+                childTransform->localPosition += speed * deltatime * direction;
+                unitState_C.traveledDistance += speed * deltatime;
+            }else
+            {
+                unitState_C.state = State::IDLE;
+                reached = true;
+            }
+        }
+        return reached;
     }
 } // gl3
--- code/game/src/systems/MovementSystem.h
+++ code/game/src/systems/MovementSystem.h
@@ -2,6 +2,12 @@
 // Created by julia on 22/07/2025.
 //
 #pragma once
+#include "CombatController.h"
+#include "CombatController.h"
+#include "CombatController.h"
+#include "CombatController.h"
+#include "CombatController.h"
+#include "CombatController.h"
 #include "engine/ecs/System.h"
 
 using gl3::engine::sceneGraph::Transform;
@@ -12,7 +18,8 @@ public:
     explicit MovementSystem(engine::Game &game);
     void moveTo(engine::Game &game, glm::vec3 goalPosition, float deltatime);
     void update(engine::Game &game, Transform* root);
-    void move(Transform &root, glm::vec3 goalPosition, float deltatime);
+    bool moveStraight(Transform& root, glm::vec3 direction, float deltatime);
+    bool moveCurved(Transform& root, glm::vec3 goal, float compression, float deltatime);
 private:
     glm::vec3 movementVector;
 };
